d8f3d05bc2cfd01a3b37ddae28aa1ebd99575a52
Suhail-1234ahmed <suhail96ahmed@gmail.com>
2026-08-03 13:40:21 +0530

    Update route.js

--- app/api/add/route.js
+++ app/api/add/route.js
@@ -6,8 +6,15 @@ export async function POST(request) {
     const client = await clientPromise
     const db = client.db("clicktree") 
     const collection = db.collection("link")
+    //if  link alredy exits
+    const doc = await collection.findOne({handle:body.handle})
+    if(doc){
+           return Response.json({
+        success: false,error:true,message:'your link is alredy exist', result:null,
+    });
+    }
     const result = await collection.insertOne(body)
     return Response.json({
-        success: true,error:false,message:'added', result:result,
+        success: true,error:false,message:'your link has been added', result:result,
     });
 }
