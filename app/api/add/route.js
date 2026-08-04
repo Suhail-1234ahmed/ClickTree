f6616d4c510709d4ddaed9ed0e6a6290a37e0788
Suhail-1234ahmed <suhail96ahmed@gmail.com>
2026-08-04 10:22:37 +0530

    Update route.js

--- app/api/add/route.js
+++ app/api/add/route.js
@@ -1,20 +1,32 @@
 import clientPromise from "@/lib/mongodb";
 
-
 export async function POST(request) {
-    const body = await request.json()
-    const client = await clientPromise
-    const db = client.db("clicktree") 
-    const collection = db.collection("link")
-    //if  link alredy exits
-    const doc = await collection.findOne({handle:body.handle})
-    if(doc){
-           return Response.json({
-        success: false,error:true,message:'your link is alredy exist', result:null,
+    const body = await request.json();
+
+    const client = await clientPromise;
+    const db = client.db("clicktree");
+    const collection = db.collection("link");
+
+    const doc = await collection.findOne({
+        handle: body.handle
     });
+
+    if (doc) {
+        return Response.json({
+            success: false,
+            message: "Handle already exists."
+        });
     }
-    const result = await collection.insertOne(body)
+
+    const result = await collection.insertOne({
+        handle: body.handle,
+        pic: body.pic,
+        links: body.links
+    });
+
     return Response.json({
-        success: true,error:false,message:'your link has been added', result:result,
+        success: true,
+        message: "ClickTree created successfully.",
+        result
     });
 }
