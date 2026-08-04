7bbbe04d5e8a922c4872c085085adccadf9fb0e8
Suhail-1234ahmed <suhail96ahmed@gmail.com>
2026-08-04 10:22:39 +0530

    Update page.js

--- app/generate/page.js
+++ app/generate/page.js
@@ -2,11 +2,13 @@
 // import { initialize } from 'next/dist/server/lib/render-server';
 import React, { useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
+import { useSearchParams } from "next/navigation";
 
 const page = () => {
+     const searchParams = useSearchParams();
     // const[link,setlink]=useState("")
     // const [linktext, setlinktext] = useState("")
-    const [handle, sethandle] = useState("")
+    const [handle, sethandle] = useState(searchParams.get('handle'))
     const [links, setLinks] = useState([{ link: "", linktext: "" }])
     const [pic, setpic] = useState("")
 
@@ -32,14 +34,14 @@ const page = () => {
 
     }
 
-    const submitLinks = async (text, link, handle) => {
+    const submitLinks = async () => {
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
 
         const raw = JSON.stringify({
-            "link": link,
-            "linktext": text,
-            "handle": handle
+            handle,
+            pic,
+            links
         });
 
         const requestOptions = {
@@ -48,12 +50,20 @@ const page = () => {
             body: raw,
             redirect: "follow"
         };
-      
+
+    
+
 
         const r = await fetch("http://localhost:3000/api/add", requestOptions)
         const result = await r.json()
         if (result.success) {
             toast.success(result.message)
+            setLinks([
+                {
+                    link: "",
+                    linktext: ""
+                }
+            ]);
         }
         else {
             toast.error(result.message)
@@ -84,8 +94,8 @@ const page = () => {
                         <h2 className='font-semibold text-2xl'>Step 2: Add Links</h2>
                         {links && links.map((item, index) => {
                             return <div key={index} className='mx-4 '>
-                                <input value={item.link} onChange={(e) => handlechange(index, e.target.value, item.linktext)} className='px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-full bg-white' type="text" placeholder='Enter link ' />
                                 <input value={item.linktext} onChange={(e) => handlechange(index, item.link, e.target.value)} className='px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-full bg-white' type="text" placeholder='Enter link text' />
+                                <input value={item.link} onChange={(e) => handlechange(index, e.target.value, item.linktext)} className='px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-full bg-white' type="text" placeholder='Enter link ' />
                             </div>
                         })}
                         <button onClick={() => addlink()} className='p-5 py-2 mx-2 bg-slate-900 text-white font-bold rounded-3xl'>+ Add Link</button>
@@ -96,7 +106,7 @@ const page = () => {
                         <h2 className='font-semibold text-2xl'>Step 3: Add Picture and Description</h2>
                         <div className='mx-4 flex flex-col'>
                             <input value={pic || ""} onChange={e => { setpic(e.target.value) }} className='px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-full bg-white' type="text" placeholder='Enter link to your Picture' />
-                            <button onClick={() => { links.forEach((item) => { submitLinks(item.linktext, item.link, item.hanlde) }) }} className='disabled:bg-slate-500 p-5 py-2 mx-2 w-fit my-5 bg-slate-900 text-white font-bold rounded-3xl'>Create your ClickTree</button>
+                            <button onClick={submitLinks} className='disabled:bg-slate-500 p-5 py-2 mx-2 w-fit my-5 bg-slate-900 text-white font-bold rounded-3xl'>Create your ClickTree</button>
                         </div>
                     </div>
                 </div>
